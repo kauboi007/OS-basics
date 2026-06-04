ff2f165dd1e739a9639334b2d7e3b487842ca0d2
kauboi007 <kaushikmugundan2019@gmail.com>
2026-06-04 20:07:46 +0530

    scheduler FCFS

--- scheduler.c
+++ scheduler.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-
+#include<stdlib.h>
 int max(int a,int b){
     if(a>b) return a;
     return b;
@@ -43,8 +43,22 @@ void fcfs(int n,struct process*arr){
 void print_results(int n,struct process*arr){
     int avg_time=0;
     for(int i=0;i<n;i++){
-        printf("start :%d burst:%d end:%d pid:%d turnover:%d wait:%d",arr[i].start,arr[i].burst,arr[i].finish,arr[i].pid,arr[i].turnover,arr[i].wait);
+        printf("start :%d burst:%d end:%d pid:%d turnover:%d wait:%d\n",arr[i].start,arr[i].burst,arr[i].finish,arr[i].pid,arr[i].turnover,arr[i].wait);
         avg_time+=arr[i].wait;
     }   
     printf("average wait:%d",avg_time/n);
+}
+int comp(const void*a,const void*b){
+    struct process*p1=(struct process*)a; 
+    struct process *p2=(struct process*)b;
+    return p1->start-p2->start;
+}
+int main(){
+    int n=3;
+    struct process arr[n];
+    init_process(n,arr);
+    qsort(arr,n,sizeof(struct process),comp);
+    fcfs(n,arr);
+    print_results(n,arr);
+
 }
